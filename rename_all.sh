c73a8708265a56831c563361dd1bb410c488062f
fhsantanna <fhsantanna@yahoo.com.br>
2015-09-03 00:21:55 -0300

    Update rename_all.sh

--- rename_all.sh
+++ rename_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#This script was created to modify .pep output files created in Amphoranet website. The aim is to prepare this files for subsequent concatenation using phyutils, after alignment.
+#This script was created to modify .pep output files created in Amphoranet website. The aim is to prepare these files for subsequent concatenation of alignments using phyutils. after alignment.
+#Firstly,decompress each zipped file in a folder with the name of the species.
 
 for dir in *; do
 cd $dir
@@ -8,3 +9,7 @@ cd $dir
 	done
 cd ..
 done
+
+#Fasta files from a specific gene but from different lineages cam be joined for subsequent alignment. Ex: cat *rpoB_ren.fas > rpoB_all.fasta; muscle -in rpoB_all.fasta -out rpoB_all_aln.fasta
+#Alignments can be joined using phyutils. Ex: java -jar /opt/phyutility/phyutility.jar -concat -in *.fas -out paeni_conc.nex
+
