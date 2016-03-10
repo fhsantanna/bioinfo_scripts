12eedf5519f05ce2a328d3a004a8e58798568c75
fhsantanna <fhsantanna@yahoo.com.br>
2016-03-10 14:32:14 -0300

    Added files via upload

---
+++ ppt_rnt_maker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+annotation_file = "P_riograndensis_LN831776.gbk"
+rnt_file = "P_riograndensis_LN831776.rnt"
+ptt_file = "P_riograndensis_LN831776.ptt"
+fasta_file = "P_riograndensis_LN831776.fas"
+r = SeqIO.parse(annotation_file, "gb")
+for record in r:
+   fasta_file = open(fasta_file, "a")
+   SeqIO.write(record, fasta_file, "fasta")
+   fasta_file.close()
+   record.features = [f for f in record.features if f.type == "rRNA" or f.type == "tRNA"]
+   fout = open(rnt_file, "a")
+   fout.write("{0} - 0..{1}\n".format(record.description, len(record)))
+   fout.write("{0} RNAs\n".format(len(record.features)))
+   fout.write("Location\tStrand\tLength\tPID\tGene\tSynonym Code\tCOG\tProduct\n")
+   strand = {1:'+', -1:'-'}
+   for f in record.features:
+       fout.write("{0}\n".format("\t".join([str(f.location.start)+".."+str(f.location.end),strand[f.strand],str(abs(f.location.start-f.location.end)),'-',f.qualifiers["locus_tag"][0],f.qualifiers["locus_tag"][0],"-",f.qualifiers["product"][0]])))
+   fout.close()
+
+r = SeqIO.parse(annotation_file, "gb")
+for record in r:
+   record.features = [f for f in record.features if f.type == "CDS"]
+   fout = open(ptt_file, "a")
+   fout.write("{0} - 0..{1}\n".format(record.description, len(record)))
+   fout.write("{0} proteins\n".format(len(record.features)))
+   fout.write("Location\tStrand\tLength\tPID\tGene\tSynonym Code\tCOG\tProduct\n")
+   for f in record.features:
+       fout.write("{0}\n".format("\t".join([str(f.location.start)+".."+str(f.location.end),strand[f.strand],str(abs(f.location.start-f.location.end)),'-',f.qualifiers["locus_tag"][0],f.qualifiers["locus_tag"][0],"-",f.qualifiers["product"][0]])))
+   fout.close()
