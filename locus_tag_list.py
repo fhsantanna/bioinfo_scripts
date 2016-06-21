71d2fa4394c84430c22c7c6fa3430168cfd4b6bc
fhsantanna <fhsantanna@yahoo.com.br>
2016-06-21 09:19:34 -0300

    Update locus_tag_list.py

--- locus_tag_list.py
+++ locus_tag_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Create list corresponding locus_tag to product
+#Create list corresponding locus_tag to product and location
 
 from Bio import SeqIO
 import sys
@@ -9,9 +9,8 @@ for rec in SeqIO.parse(open(sys.argv[1],"rU"),"genbank"):
     if rec.features:
         for feature in rec.features:
             if feature.type == "rRNA":
-                print "%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0])
+                print "%s\t%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0],feature.location)
             if feature.type == "CDS":
-                print "%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0])
+                print "%s\t%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0],feature.location)
             if feature.type == "tRNA":
-                print "%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0])
-
+		        print "%s\t%s\t%s" % (feature.qualifiers["locus_tag"][0],feature.qualifiers["product"][0],feature.location)
