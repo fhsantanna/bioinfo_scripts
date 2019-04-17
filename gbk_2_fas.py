d439da6276ecf3622c40f1b12641f5d833166ef2
fhsantanna <fhsantanna@yahoo.com.br>
2019-04-17 15:42:43 -0300

    Update gbk_2_fas.py

--- gbk_2_fas.py
+++ gbk_2_fas.py
@@ -5,8 +5,7 @@ import sys
 
 
 for seq_record in SeqIO.parse(sys.stdin, "genbank"):
-	with open("{}.fasta".format(str(seq_record.description)), "w") as output_handle:
-		output_handle.write(">%s\n%s\n" % (
-            seq_record.description,
-            seq_record.seq))
-print("Converted {} records".format(seq_record.id))
+	with open("{}.fasta".format(str(seq_record.id)), "w") as output_handle:
+		count = SeqIO.write(seq_record, output_handle, "fasta")
+
+print("Converted %i records" % count)
