c80046ff2d44c29ea1fd5db994d85d5e34040a78
fhsantanna <fhsantanna@yahoo.com.br>
2021-07-16 16:07:07 -0300

    Add files via upload

--- check_mut.py
+++ check_mut.py
@@ -9,13 +9,8 @@ import sys
 file_name = sys.argv[1]
 alignment = AlignIO.read(file_name, "fasta")
 
-y = 0
 
-print("Id", "File", "Pos", "Sequence_1", "NT_1", "NT_2", "Sequence_2")
+print("File", "Pos", "Sequence_1", "NT_1", "NT_2", "Sequence_2")
 for r in range(0,len(alignment[1].seq)):
      if alignment[0,r] != alignment[1,r]:
-        if alignment[0,r] != alignment[1,r]:
-            y = y + 1
-            print(y, file_name, r, alignment[0].name, alignment[0,r], alignment[1,r], alignment[1].name)
-        else:
-            y = 0
+        print(file_name, r+1, alignment[0].name, alignment[0,r], alignment[1,r], alignment[1].name)
